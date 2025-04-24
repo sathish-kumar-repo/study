3e2099b51930187bcdf98c9daa891142469e1167
sathish-kumar-repo <sathishkumar.mailhub@gmail.com>
2025-04-24 22:26:00 +0530

    updated

--- src/Components/Section.tsx
+++ src/Components/Section.tsx
@@ -1,12 +1,11 @@
 import styled from "styled-components";
 
 const Section = styled.section`
-  background: url("/web-images/bg.jpg");
+  background: url("/study/web-images/bg.jpg");
   background-size: cover;
   background-position: center;
   background-attachment: fixed;
   min-height: 100vh;
-  // width: 100vw;
 `;
 
 export default Section;
