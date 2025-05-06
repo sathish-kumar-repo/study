0109221d600a50552c17862ed9d3c285b6b01766
sathish-kumar-repo <sathishkumar.mailhub@gmail.com>
2025-05-06 07:15:15 +0530

    updated

--- src/screen/LifeStyle/BasicRulesToLiveHealthyLifeStyle/code/index.tsx
+++ src/screen/LifeStyle/BasicRulesToLiveHealthyLifeStyle/code/index.tsx
@@ -1,9 +1,25 @@
-import { Title } from "../../../../components/UI";
+import { H1, List, Title } from "../../../../components/UI";
 
 const Rules = () => {
   return (
     <>
       <Title>Basic Rules to Live Healthy Lifestyle</Title>
+
+      <H1>Follow Rules</H1>
+      <List
+        type="ordered"
+        items={[
+          {
+            text: "Proper day routine",
+          },
+          { text: "Stop any negative addiction" },
+          { text: "Train mentally and physically" },
+          {
+            text: "Optimize your intake",
+          },
+          { text: "Don’t be in comfort zone" },
+        ]}
+      />
     </>
   );
 };
