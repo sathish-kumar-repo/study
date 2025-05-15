5f15f4e353b80f13b1ac4fa0d47d3ab9f9c75967
sathish-kumar-repo <sathishkumar.mailhub@gmail.com>
2025-05-15 21:56:22 +0530

    sdsds

--- src/Components/Section.tsx
+++ src/Components/Section.tsx
@@ -1,8 +1,7 @@
 import styled from "styled-components";
-import img from "../assets/img/bg3.jpg";
 
 const Section = styled.section`
-  background: url(${img});
+  background: url("/study/web-images/bg.jpg");
   background-size: cover;
   background-position: center;
   background-attachment: fixed;
