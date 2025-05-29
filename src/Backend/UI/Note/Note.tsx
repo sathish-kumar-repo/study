7cd19ea62cef049a3f53ea69b7f0b829b04adb61
sathish-kumar-repo <sathishkumar.mailhub@gmail.com>
2025-05-29 12:12:07 +0530

    update note component

--- src/Backend/UI/Note/Note.tsx
+++ src/Backend/UI/Note/Note.tsx
@@ -3,13 +3,31 @@ import "./Note.css";
 
 interface NoteProps {
   children?: React.ReactNode;
+  link?: string;
 }
 
-const Note = ({ children }: NoteProps) => {
+const Note = ({ children, link }: NoteProps) => {
+  if (children && link) {
+    throw new Error(
+      "Note component cannot have both 'children' and 'link' props."
+    );
+  }
+
   return (
     <div className="note-container">
       <h3>Note:</h3>
-      <div className="notes">{children}</div>
+      <div className="notes">
+        {link ? (
+          <>
+            Click here to learn more{" "}
+            <a href={link} target="_blank" rel="noopener noreferrer">
+              {link}
+            </a>
+          </>
+        ) : (
+          children
+        )}
+      </div>
     </div>
   );
 };
