6ad3f84d09aa3d9055e028dc77b2dcd153e8685b
sathish-kumar-repo <sathishkumar.mailhub@gmail.com>
2025-06-15 17:18:49 +0530

    update 'src/Frontend/Life Style/Workout Routine/code/Stretch/index.tsx'

--- src/Frontend/Life Style/Workout Routine/code/Stretch/index.tsx
+++ src/Frontend/Life Style/Workout Routine/code/Stretch/index.tsx
@@ -8,6 +8,11 @@ const Stretch = () => {
       <Media src="https://youtube.com/shorts/sGdRJxUdPN8?si=DnWMbVIaQtrZSa5q" />
       <H1>Lower Body</H1>
       <Media src="https://youtube.com/shorts/LG1ZV62zTWI?si=dnuvMuqcADeDGzsS" />
+      <H1>
+        Ultimate Recovery Stretch Routine for Faster Muscle Recovery &
+        Flexibility
+      </H1>
+      <Media src="https://youtube.com/shorts/pNvXq37fSqo?si=5vDBe2rbqCgHCeOR" />
     </>
   );
 };
